970d2c234869757e455963e844be6fe72c999cd9
[Cloud3] 신준영 <2261-jy.shin@users.noreply.kdt-gitlab.elice.io>
2024-06-14 09:51:02 +0900

    Squashed commit of the following:
    commit 92624751b77280fdab0326dbd08d4b10d0bfa420
    Author: minh <rnrnck78@naver.com>
    Date:   Thu Jun 13 19:16:44 2024 +0900
    
        fix : access -> token으로 이름 변경
    
    commit ba50a87acf373dba5772d8ab5efe2633176ad4b5
    Author: minh <rnrnck78@naver.com>
    Date:   Thu Jun 13 18:30:26 2024 +0900
    
        feat : 로그아웃 기능 구현
    
    commit 7dfb4f894c221178b035a9cafba313e5e2d56003
    Merge: fcbc311 d3c7be9
    Author: [Cloud3] 안민형 <alsgud000727@gmail.com>
    Date:   Wed Jun 12 13:36:32 2024 +0000
    
        Merge branch 'feature-FE-user-ann-feat-paging' into 'dev'
    
        feat : 관리자 회원관리 페이지네이션 적용
    
    commit d3c7be94f1d999c08f2c6d4d8474fa910b7a75d8
    Author: minh <rnrnck78@naver.com>
    Date:   Wed Jun 12 22:32:54 2024 +0900
    
        feat : 관리자 회원관리 페이지네이션 적용

--- src/views/src/components/home/HomeHeader.js
+++ src/views/src/components/home/HomeHeader.js
@@ -5,11 +5,13 @@ import MenuButton from "../../resources/home/header/menu.png";
 import CartButton from "../../resources/home/header/Cart.png";
 import UserButton from "../../resources/home/header/user.png";
 import './HomeHeader.css';
+import Logout from '../../routes/user/auth/logout';
+import { useState } from 'react';
+
 
 function HomeHeader()  {
     // 장바구니: 로그인 안 했을 경우, 로그인 페이지로 이동
     const history = useHistory();
-    console.log(localStorage.getItem('token'));
     const cartClick = () => {
         const token = localStorage.getItem('token');
         const userName = localStorage.getItem('userName');
@@ -19,14 +21,13 @@ function HomeHeader()  {
     return(
         <div className="home-header">
             <div id="title">
-               <Link to="/"><h1>BookFlow</h1></Link>
+                <Link to="/"><h1>BookFlow</h1></Link>
             </div>
             <div id="icons">
                 <Link to="/search"><img src={SearchButton} /></Link>
                 <img src={CartButton} onClick={cartClick} />
-
                 <Link to="/login"><img src={UserButton} /></Link>
-
+                <Logout />
                 <Link to="/menu"><img src={MenuButton} /></Link>
             </div>
         </div>
