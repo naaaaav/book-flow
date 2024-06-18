02aeae76d626ccf6afc6ceee2a8b66eb5295ca3f
[Cloud3] 신준영 <2261-jy.shin@users.noreply.kdt-gitlab.elice.io>
2024-06-18 17:12:13 +0900

    홈헤더 사이트로고 좌측 마진 제거

--- src/views/src/components/home/HomeHeader.js
+++ src/views/src/components/home/HomeHeader.js
@@ -49,7 +49,7 @@ function HomeHeader()  {
       return (
         <Flex className="home-header" align="center" justify="space-between" p="2">
           <Link as={RouterLink} to="/" display="flex" alignItems="center">
-            <Heading as="h1" ml="2" my="auto" size="lg">BookFlow</Heading>
+            <Heading as="h1" my="auto" size="lg">BookFlow</Heading>
           </Link>
           <Flex id="icons" justify="space-around" align="center">
             <Link as={RouterLink} to="/search">
